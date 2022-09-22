09180f3361b319dc80723b3f942bb6b0867eda70
Yoo Mina <110373500+maydec2298@users.noreply.github.com>
2022-09-22 22:16:08 +0900

    feat : 서버 주소 변경

--- src/shared/api.js
+++ src/shared/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // axios 기본 주소 & header 타입 세팅
 export const api = axios.create({
-  baseURL: "http://15.165.100.67/api",
+  baseURL: "http://13.124.170.188/api",
   headers: {
     "Content-Type": "application/json",
   },
