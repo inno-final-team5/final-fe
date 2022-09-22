d98a5b106931aca5808f06a9d36bbffdfd3df99a
heyley <hyelimiam@gmail.com>
2022-09-22 22:12:38 +0900

    style:detail view

---
+++ src/shared/api.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+// axios 기본 주소 & header 타입 세팅
+export const api = axios.create({
+  baseURL: 'http://15.165.100.67/api/',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export default api;
+
+api.interceptors.request.use(function (config) {
+  const accessToken = localStorage.getItem('accessToken');
+  config.headers.common['accessToken'] = `${accessToken}`;
+  return config;
+});
