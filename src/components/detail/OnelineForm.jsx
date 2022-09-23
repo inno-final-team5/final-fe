3577974caf606735b4bde5d6171fe84675aa3d8f
heyley <hyelimiam@gmail.com>
2022-09-23 21:44:00 +0900

    Feat:view completed

--- src/components/detail/OnelineForm.jsx
+++ src/components/detail/OnelineForm.jsx
@@ -1,6 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import { FaStar } from 'react-icons/fa';
-import styled from 'styled-components';
+import React, { useState, useEffect } from "react";
+import { FaStar } from "react-icons/fa";
+import { BsTrash } from "react-icons/bs";
+import { TiPencil } from "react-icons/ti";
+import styled from "styled-components";
 
 const OnelineForm = () => {
   const [clicked, setClicked] = useState([false, false, false, false, false]);
@@ -34,25 +36,50 @@ const OnelineForm = () => {
 
   return (
     <div>
-      <section className="">
-        <div className="container px-5 py-24 mx-auto flex items-center md:flex-row flex-col">
-          <div className="flex items-center md:pr-10 md:mb-0 mb-4 pr-2 w-full md:w-auto md:text-center ">
+      <section className="mt-6">
+        <div className="container pl-2 pt-2 pb-2 rounded-3xl bg-mGray mx-auto flex flex-wrap flex-col md:flex-row items-center">
+          <div className="flex title-font font-medium items-center ml-2 mr-2 mb-4 md:mb-0">
             <h1 className="md:text-xl font-medium title-font md:flex-row flex-col text-mYellow">한줄평작성하기</h1>
           </div>
           <Stars>
             {array.map((el, idx) => {
-              return <FaStar key={idx} size="30" onClick={() => handleStarClick(el)} className={clicked[el] && 'yellowStar'} />;
+              return <FaStar key={idx} size="30" onClick={() => handleStarClick(el)} className={clicked[el] && "yellowStar"} />;
             })}
           </Stars>
-          <div className="p-6 flex">
-            <input className="leading-10" />
+          <div className="flex md:w-3/4 w-3/4 2xl:w-3/4 xl:w-2/3 lg:w-1/2 md:w-2/3 space-x-6">
+            <div className="2xl:w-full md:w-full xl:w-full lg:w-full md:mr-auto md:ml-2 md:py-2 md:pl-8 md:border-l md:border-gray-400	flex flex-wrap text-base ">
+              <input className="leading-10 w-full rounded-xl" />
+            </div>
+            <div className="flex-shrink-0 inline-flex items-center focus:outline-none text-base xl:mr-6 md:mt-0">
+              <button className="2xl:px-6 xl:px-10 lg:px-6 md:px-10 bg-mYellow inline-flex py-3 rounded-full items-center hover:bg-mCream ">
+                작성하기
+              </button>
+            </div>
           </div>
-          <div className="flex md:ml-auto md:mr-0 mx-auto items-center flex-shrink-0 space-x-4">
-            <button className="bg-gray-100 inline-flex py-3 px-4 rounded-lg items-center hover:bg-gray-200 focus:outline-none">
-              <span className="ml-2 flex items-start flex-col leading-none ">
-                <span className="title-font font-medium ">작성하기</span>
-              </span>
-            </button>
+        </div>
+      </section>
+      <section className="mt-6">
+        <div className="container pt-2 pb-2 rounded-3xl bg-mGray mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+          <div className="flex title-font font-medium items-center mr-4 mb-4 md:mb-0">
+            <h1 className="p-2 md:text-xl font-medium title-font md:flex-row flex-col text-mYellow">내가쓴한줄평</h1>
+          </div>
+          <div className="flex md:w-1/2 2xl:w-full xl:w-full md:w-full space-x-2">
+            <Stars className="mt-7 ml-3">
+              {array.map((el, idx) => {
+                return <FaStar key={idx} size="30" onClick={() => handleStarClick(el)} className={clicked[el] && "yellowStar"} />;
+              })}
+            </Stars>
+            <div className="2xl:w-full md:w-full md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap text-base ">
+              <textarea className="leading-10 w-full bg-gray-400 rounded-xl" />
+            </div>
+            <div className="flex-shrink-0 gap-4 inline-flex items-center focus:outline-none text-base xl:mr-6 md:mt-0">
+              <button className="2xl:px-6 xl:px-6 lg:px-6 md:px-10 bg-mYellow inline-flex py-3 rounded-full items-center hover:bg-mCream ">
+                <TiPencil size="20" />
+              </button>
+              <button className="2xl:px-6 xl:px-6 lg:px-6 md:px-10 bg-mYellow inline-flex py-3 rounded-full items-center hover:bg-mCream ">
+                <BsTrash size="20" />
+              </button>
+            </div>
           </div>
         </div>
       </section>
--- src/components/detail/OnelineList.jsx
+++ src/components/detail/OnelineList.jsx
@@ -1,7 +1,65 @@
-import React from 'react';
+import React from "react";
+import { FaRegThumbsUp } from "react-icons/fa";
+import { FaThumbsUp } from "react-icons/fa";
 
 function OnelineList() {
-  return <div>OnelineList</div>;
+  return (
+    <div className="mt-6 pb-6 ">
+      <div className="pr-14 pt-0 pb-4 rounded-3xl bg-mGray container mx-auto flex px-2 py-24 md:flex-row flex-col items-center">
+        <section class="text-gray-600 body-font overflow-hidden">
+          <div class="container px-1 py-10 mx-auto">
+            <div class="-my-8 divide-y-2 divide-gray-100">
+              <div class="py-6 flex flex-wrap md:flex-nowrap">
+                <div class="px-16 md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+                  <span class="text-lg text-mCream">댓글작성자</span>
+                </div>
+                <div class="md:flex-grow flex space-x-4">
+                  <h2 class="text-lg font-medium text-gray-300 title-font mb-1">
+                    한줄평 내용입니다 한줄평 내용입니다 한줄평 내용입니다 한줄평 내용입니다 한줄평 내용입니다 한줄평 내용입니다 한줄평 내용입니다
+                    한줄평 내용입니다 한줄평 내용입니다 한줄평 내용입니다 한줄평 내용입니다
+                  </h2>
+                  <p class="text-mYellow items-center mt-2">
+                    <FaRegThumbsUp size={30} />
+                    <p className="mt-2">100</p>
+                  </p>
+                </div>
+              </div>
+              <div class="py-8 flex flex-wrap md:flex-nowrap">
+                <div class="px-16 md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+                  <span class="text-lg text-mCream">댓글작성자</span>
+                </div>
+                <div class="md:flex-grow flex space-x-4">
+                  <h2 class="text-lg font-medium text-gray-300 title-font mb-1">
+                    한줄평 내용입니다 한줄평 내용입니다 한줄평 내용입니다 한줄평 내용입니다 한줄평 내용입니다 한줄평 내용입니다 한줄평 내용입니다
+                    한줄평 내용입니다 한줄평 내용입니다 한줄평 내용입니다 한줄평 내용입니다
+                  </h2>
+                  <p class="text-mYellow items-center mt-2">
+                    <FaThumbsUp size={30} />
+                    <p className="mt-2">100</p>
+                  </p>
+                </div>
+              </div>
+              <div class="py-8 flex flex-wrap md:flex-nowrap">
+                <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+                  <span class="px-16 text-lg text-mCream">댓글작성자</span>
+                </div>
+                <div class="md:flex-grow flex space-x-4">
+                  <h2 class="text-lg font-medium text-gray-300 title-font mb-1">
+                    한줄평 내용입니다 한줄평 내용입니다 한줄평 내용입니다 한줄평 내용입니다 한줄평 내용입니다 한줄평 내용입니다 한줄평 내용입니다
+                    한줄평 내용입니다 한줄평 내용입니다 한줄평 내용입니다 한줄평 내용입니다
+                  </h2>
+                  <p class="text-mYellow items-center mt-2">
+                    <FaThumbsUp size={30} />
+                    <p className="mt-2">100</p>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
 }
 
 export default OnelineList;
