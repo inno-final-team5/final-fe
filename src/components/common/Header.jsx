a80d96e461e6395ea46611b56306451914331ec9
Yoo Mina <110373500+maydec2298@users.noreply.github.com>
2022-09-24 19:56:56 +0900

    feat : 로그인시 헤더 변경 제작 중

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,23 +1,65 @@
-import React from "react";
+import React, { useEffect } from "react";
 import LogoBox from "./LogoBox";
 import tw from "tailwind-styled-components/";
+import { NavLink } from "react-router-dom";
+import { FaBell } from "react-icons/fa";
 
 const Header = () => {
+  const logout = () => {
+    window.localStorage.removeItem("refreshToken");
+    window.localStorage.removeItem("accessToken");
+  };
+  // useEffect(() => {
+  //   logout();
+  // }, []);
   return (
-    <div className="px-5 flex items-center">
-      <LogoBox />
-      <div className="w-full ml-5 py-2.5 flex items-center justify-between border border-solid border-mYellow">
-        <ul className="flex ml-10">
-          <MenuTitle>영화 검색</MenuTitle>
-          <MenuTitle>커뮤니티</MenuTitle>
-          <MenuTitle>챌린지</MenuTitle>
-          <MenuTitle>마이 페이지</MenuTitle>
-        </ul>
-        <ul className="flex mr-10">
-          <MenuTitle>로그인</MenuTitle>
-          <MenuTitle>회원가입</MenuTitle>
-        </ul>
-      </div>
+    <div>
+      {localStorage.length ? (
+        <div className="flex items-center">
+          <LogoBox />
+          <div className="w-full ml-4 py-2.5 flex items-center justify-between border border-none bg-mGray rounded-lg">
+            <ul className="flex ml-10">
+              <MenuTitle>영화 검색</MenuTitle>
+              <MenuTitle>커뮤니티</MenuTitle>
+              <MenuTitle>챌린지</MenuTitle>
+              <MenuTitle>
+                <NavLink to="/mypage/favorites">마이 페이지</NavLink>
+              </MenuTitle>
+            </ul>
+            <ul className="flex mr-10">
+              <MenuTitle>## 평론가님 환영합니다!</MenuTitle>
+              <MenuTitle
+                onClick={() => {
+                  logout();
+                }}
+              >
+                로그아웃
+              </MenuTitle>
+              <MenuTitle>
+                <FaBell />
+              </MenuTitle>
+            </ul>
+          </div>
+        </div>
+      ) : (
+        <div className="flex items-center">
+          <LogoBox />
+          <div className="w-full ml-4 py-2.5 flex items-center justify-between border border-none bg-mGray rounded-lg">
+            <ul className="flex ml-10">
+              <MenuTitle>영화 검색</MenuTitle>
+              <MenuTitle>커뮤니티</MenuTitle>
+              <MenuTitle>챌린지</MenuTitle>
+              <MenuTitle>
+                <NavLink to="/mypage/favorites">마이 페이지</NavLink>
+              </MenuTitle>
+            </ul>
+            <ul className="flex mr-10">
+              <MenuTitle>로그인</MenuTitle>
+              <MenuTitle>회원가입</MenuTitle>
+            </ul>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
