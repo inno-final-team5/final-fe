84a69f5f34cfee76b8bdb8e6b6436b4856e8423a
heyley <hyelimiam@gmail.com>
2022-09-25 12:23:55 +0900

    Style:margin

--- src/components/detail/OnelineList.jsx
+++ src/components/detail/OnelineList.jsx
@@ -5,15 +5,15 @@ import { FaThumbsUp } from "react-icons/fa";
 function OnelineList() {
   return (
     <div className="mt-6 pb-6 ">
-      <div className="pr-14 pt-0 pb-4 rounded-3xl bg-mGray container mx-auto flex px-2 py-24 md:flex-row flex-col items-center">
+      <div className=" pr-10 pt-0 pb-4 rounded-3xl bg-mGray container mx-auto flex px-2 py-24 md:flex-row flex-col items-center">
         <section class="text-gray-600 body-font overflow-hidden">
-          <div class="container px-1 py-10 mx-auto">
-            <div class="-my-8 divide-y-2 divide-gray-100">
+          <div class="container px-1 py-10 mx-auto ">
+            <div class="-my-6 divide-y-2 divide-gray-100 ">
               <div class="py-6 flex flex-wrap md:flex-nowrap">
-                <div class="px-16 md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+                <div class="px-20 md:w-60 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
                   <span class="text-lg text-mCream">댓글작성자</span>
                 </div>
-                <div class="md:flex-grow flex space-x-4">
+                <div class="md:flex-grow flex">
                   <h2 class="text-lg font-medium text-gray-300 title-font mb-1">
                     한줄평 내용입니다 한줄평 내용입니다 한줄평 내용입니다 한줄평 내용입니다 한줄평 내용입니다 한줄평 내용입니다 한줄평 내용입니다
                     한줄평 내용입니다 한줄평 내용입니다 한줄평 내용입니다 한줄평 내용입니다
@@ -24,11 +24,11 @@ function OnelineList() {
                   </p>
                 </div>
               </div>
-              <div class="py-8 flex flex-wrap md:flex-nowrap">
-                <div class="px-16 md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+              <div class="py-6 flex flex-wrap md:flex-nowrap">
+                <div class="px-20 md:w-60 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
                   <span class="text-lg text-mCream">댓글작성자</span>
                 </div>
-                <div class="md:flex-grow flex space-x-4">
+                <div class="md:flex-grow flex ">
                   <h2 class="text-lg font-medium text-gray-300 title-font mb-1">
                     한줄평 내용입니다 한줄평 내용입니다 한줄평 내용입니다 한줄평 내용입니다 한줄평 내용입니다 한줄평 내용입니다 한줄평 내용입니다
                     한줄평 내용입니다 한줄평 내용입니다 한줄평 내용입니다 한줄평 내용입니다
@@ -39,11 +39,11 @@ function OnelineList() {
                   </p>
                 </div>
               </div>
-              <div class="py-8 flex flex-wrap md:flex-nowrap">
-                <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-                  <span class="px-16 text-lg text-mCream">댓글작성자</span>
+              <div class="py-6 flex flex-wrap md:flex-nowrap">
+                <div class="md:w-60 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+                  <span class="px-20 text-lg text-mCream">댓글작성자</span>
                 </div>
-                <div class="md:flex-grow flex space-x-4">
+                <div class="md:flex-grow flex">
                   <h2 class="text-lg font-medium text-gray-300 title-font mb-1">
                     한줄평 내용입니다 한줄평 내용입니다 한줄평 내용입니다 한줄평 내용입니다 한줄평 내용입니다 한줄평 내용입니다 한줄평 내용입니다
                     한줄평 내용입니다 한줄평 내용입니다 한줄평 내용입니다 한줄평 내용입니다
