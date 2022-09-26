aa6c789ebf91b1a3841f54cd625e0da19e4d52cb
heyley <hyelimiam@gmail.com>
2022-09-27 00:34:52 +0900

    feat:reactQuery active

--- src/components/search/SearchList.jsx
+++ src/components/search/SearchList.jsx
@@ -1,50 +1,29 @@
 import React from "react";
 import poster from "../../images/poster.png";
+import Spinner from "components/common/Spinner";
+import axios from "axios";
+import { useQuery } from "react-query";
+import SearchMovie from "./SearchMovie";
 
 const SearchList = () => {
+  const getSearchList = () => {
+    return axios.get("http://localhost:3001/movieList");
+  };
+
+  const searchListQuery = useQuery("searchList", getSearchList, {
+    onSuccess: (data) => {},
+  });
+  if (searchListQuery.isLoading) {
+    return <Spinner />;
+  }
+
   return (
     <div className="mt-10">
       <div className="flex items-center justify-center pt-0 pb-4 rounded-3xl bg-mGray container mx-auto flex px-2 py-22 md:flex-row flex-col">
         <section class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-14 p-14">
-          <div class="relative cursor-pointer">
-            <a
-              href="#!"
-              class="absolute inset-0 z-10 text-center flex flex-col items-center justify-center opacity-0 hover:opacity-80 bg-gray-800 duration-300"
-            >
-              <h1 className="text-2xl text-mCream font-semibold">해리포터</h1>
-            </a>
-            <div class="relative flex flex-wrap content-center">
-              <img alt="영화포스터" src={poster} />
-            </div>
-          </div>
-          <div class="relative cursor-pointer">
-            <a
-              href="#!"
-              class="absolute inset-0 z-10 text-center flex flex-col items-center justify-center opacity-0 hover:opacity-80 bg-gray-800 duration-300"
-            >
-              <h1 className="text-2xl text-mCream font-semibold">해리포터</h1>
-            </a>
-            <div class="relative flex flex-wrap content-center">
-              <img alt="영화포스터" src={poster} />
-            </div>
-          </div>
-          <div class="relative cursor-pointer">
-            <a
-              href="#!"
-              class="absolute inset-0 z-10 text-center flex flex-col items-center justify-center opacity-0 hover:opacity-80 bg-gray-800 duration-300"
-            >
-              <h1 className="text-2xl text-mCream font-semibold">해리포터</h1>
-            </a>
-            <div class="relative flex flex-wrap content-center">
-              <img alt="영화포스터" src={poster} />
-            </div>
-          </div>
-          <img alt="영화포스터" src={poster} />
-          <img alt="영화포스터" src={poster} />
-          <img alt="영화포스터" src={poster} />
-          <img alt="영화포스터" src={poster} />
-          <img alt="영화포스터" src={poster} />
-          <img alt="영화포스터" src={poster} />
+          {searchListQuery.data.data.map((movie) => (
+            <SearchMovie {...movie} key={movie.movieId} />
+          ))}
         </section>
       </div>
     </div>
