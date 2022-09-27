dfd3df39e665e475f57e6a55d2b966e813beb3f0
Yoo Mina <110373500+maydec2298@users.noreply.github.com>
2022-09-27 20:27:54 +0900

    feat : boxoffice, 최신리뷰 get json server 연결

--- src/components/mainpage/BoxOfiice.jsx
+++ src/components/mainpage/BoxOfiice.jsx
@@ -4,11 +4,11 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-// import tw from "tailwind-styled-components";
-
-import poster from "../../images/movie_poster_sample.jpg";
+import { useQuery } from "react-query";
+import api from "shared/api";
 
 const BoxOfiice = () => {
+  //carousel
   const settings = {
     dots: false,
     infinite: true,
@@ -18,149 +18,169 @@ const BoxOfiice = () => {
     autoplay: true,
     autoplaySpeed: 2000,
   };
+  const getBoxofficeWithApi = async () => {
+    const { data } = await api.get("/boxoffice");
+    return data;
+  };
+
+  const query = useQuery("boxoffice", getBoxofficeWithApi);
+  console.log(query);
+
   return (
     <div className="w-11/12 m-auto ">
-      <h2 className="text-mYellow text-2xl my-5"> BOX OFFICE </h2>
+      <h2 className="text-mYellow text-2xl my-5 font-bold"> BOX OFFICE </h2>
+
       <Slider {...settings} className="flex pr-10">
+        {!query.isLoading &&
+          query.data.map((i) => (
+            <div key={i.movieId} className="relative  h-72">
+              <div className="absolute top-0 left-4 mb-1 text-7xl font-bold text-mCream z-10">
+                {i.movieId}
+              </div>
+              <img
+                src={i.imageUrl}
+                alt="movie_poster"
+                className="relative top-7 left-16 w-40 z-40"
+              />
+              <div className=" absolute left-16 bottom-0 text-mYellow  z-10">
+                {i.title}
+              </div>
+            </div>
+          ))}
+        {/*         
         <div className="relative  h-72">
-          <div className="absolute top-0 left-4 mb-1 text-8xl font-bold text-mYellow z-10">
+          <div className="absolute top-0 left-4 mb-1 text-7xl font-bold text-mYellow z-10">
             1
           </div>
           <img
             src={poster}
             alt="movie_poster"
-            className="absolute top-7 left-16 w-40 z-40"
+            className="relative top-7 left-16 w-40 z-40"
           />
-          <div className="absolute left-16 bottom-0 text-mYellow my-2.5 z-10">
+          <div className=" absolute left-16 bottom-0 text-mYellow my-2.5 z-10">
             라라랜드
           </div>
         </div>
-
         <div className="relative  h-72">
-          <div className="absolute top-0 left-4 mb-1 text-8xl font-bold text-mYellow z-10">
+          <div className="absolute top-0 left-4 mb-1 text-7xl font-bold text-mYellow z-10">
             2
           </div>
           <img
-            src={poster}
+            src={poster2}
             alt="movie_poster"
-            className="absolute top-7 left-16 w-40 z-40"
+            className="relative top-7 left-16 w-40 z-40"
           />
           <div className="absolute left-16 bottom-0 text-mYellow my-2.5 z-10">
-            라라랜드
+            해리포터와 마법사의 돌
           </div>
         </div>
-
         <div className="relative  h-72">
-          <div className="absolute top-0 left-4 mb-1 text-8xl font-bold text-mYellow z-10">
+          <div className="absolute top-0 left-4 mb-1 text-7xl font-bold text-mYellow z-10">
             3
           </div>
           <img
             src={poster}
             alt="movie_poster"
-            className="absolute top-7 left-16 w-40 z-40"
+            className="relative top-7 left-16 w-40 z-40"
           />
           <div className="absolute left-16 bottom-0 text-mYellow my-2.5 z-10">
             라라랜드
           </div>
         </div>
-
         <div className="relative  h-72">
-          <div className="absolute top-0 left-4 mb-1 text-8xl font-bold text-mYellow z-10">
+          <div className="absolute top-0 left-4 mb-1 text-7xl font-bold text-mYellow z-10">
             4
           </div>
           <img
-            src={poster}
+            src={poster2}
             alt="movie_poster"
-            className="absolute top-7 left-16 w-40 z-40"
+            className="relative top-7 left-16 w-40 z-40"
           />
           <div className="absolute left-16 bottom-0 text-mYellow my-2.5 z-10">
-            라라랜드
+            해리포터와 마법사의 돌
           </div>
         </div>
-
         <div className="relative  h-72">
-          <div className="absolute top-0 left-4 mb-1 text-8xl font-bold text-mYellow z-10">
+          <div className="absolute top-0 left-4 mb-1 text-7xl font-bold text-mYellow z-10">
             5
           </div>
           <img
             src={poster}
             alt="movie_poster"
-            className="absolute top-7 left-16 w-40 z-40"
+            className="relative top-7 left-16 w-40 z-40"
           />
           <div className="absolute left-16 bottom-0 text-mYellow my-2.5 z-10">
             라라랜드
           </div>
         </div>
-
         <div className="relative  h-72">
-          <div className="absolute top-0 left-4 mb-1 text-8xl font-bold text-mYellow z-10">
+          <div className="absolute top-0 left-4 mb-1 text-7xl font-bold text-mYellow z-10">
             6
           </div>
           <img
-            src={poster}
+            src={poster2}
             alt="movie_poster"
-            className="absolute top-7 left-16 w-40 z-40"
+            className="relative top-7 left-16 w-40 z-40"
           />
           <div className="absolute left-16 bottom-0 text-mYellow my-2.5 z-10">
-            라라랜드
+            해리포터와 마법사의 돌
           </div>
         </div>
-
         <div className="relative  h-72">
-          <div className="absolute top-0 left-4 mb-1 text-8xl font-bold text-mYellow z-10">
+          <div className="absolute top-0 left-4 mb-1 text-7xl font-bold text-mYellow z-10">
             7
           </div>
           <img
             src={poster}
             alt="movie_poster"
-            className="absolute top-7 left-16 w-40 z-40"
+            className="relative top-7 left-16 w-40 z-40"
           />
           <div className="absolute left-16 bottom-0 text-mYellow my-2.5 z-10">
             라라랜드
           </div>
         </div>
-
         <div className="relative  h-72">
-          <div className="absolute top-0 left-4 mb-1 text-8xl font-bold text-mYellow z-10">
+          <div className="absolute top-0 left-4 mb-1 text-7xl font-bold text-mYellow z-10">
             8
           </div>
           <img
-            src={poster}
+            src={poster2}
             alt="movie_poster"
-            className="absolute top-7 left-16 w-40 z-40"
+            className="relative top-7 left-16 w-40 z-40"
           />
           <div className="absolute left-16 bottom-0 text-mYellow my-2.5 z-10">
-            라라랜드
+            해리포터와 마법사의 돌
           </div>
         </div>
-
         <div className="relative  h-72">
-          <div className="absolute top-0 left-4 mb-1 text-8xl font-bold text-mYellow z-10">
+          <div className="absolute top-0 left-4 mb-1 text-7xl font-bold text-mYellow z-10">
             9
           </div>
           <img
             src={poster}
             alt="movie_poster"
-            className="absolute top-7 left-16 w-40 z-40"
+            className="relative top-7 left-16 w-40 z-40"
           />
           <div className="absolute left-16 bottom-0 text-mYellow my-2.5 z-10">
             라라랜드
           </div>
         </div>
-
         <div className="relative  h-72">
-          <div className="absolute top-3 left-0 mb-1 text-7xl font-bold text-mYellow z-10">
+          <div className="absolute top-0 left-0 mb-1 text-7xl font-bold text-mYellow z-10">
             10
           </div>
           <img
-            src={poster}
+            src={poster2}
             alt="movie_poster"
-            className="absolute top-7 left-16 w-40 z-40"
+            className="relative top-7 left-16 w-40 z-40"
           />
           <div className="absolute left-16 bottom-0 text-mYellow my-2.5 z-10">
-            라라랜드
+            해리포터와 마법사의 돌
           </div>
-        </div>
+        </div> */}
+
+        {/* ver2 */}
+
         {/* <div>
           <div className="mb-1 text-base text-mYellow underline underline-offset-4">
             2
