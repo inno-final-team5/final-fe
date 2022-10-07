4889e5c69f86346f6e146a99bc097ec8184089df
heyley <hyelimiam@gmail.com>
2022-10-07 11:31:28 +0900

    feat:oneline none

--- src/components/detail/OnelineList.jsx
+++ src/components/detail/OnelineList.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Spinner from "components/common/Spinner";
 import { useQuery } from "react-query";
 import Oneline from "./Oneline";
@@ -8,30 +8,42 @@ import { useParams } from "react-router-dom";
 function OnelineList() {
   const params = useParams();
   const id = params.id;
-
+  const [success, setSuccess] = useState([]);
   /**한줄평 리스트 불러오기 */
   const getOnelineList = () => {
     return api.get(`/movie/${id}/one-line-review`);
   };
   const onelineQuery = useQuery("onelineList", getOnelineList, {
-    onSuccess: (data) => {},
+    onSuccess: (data) => {
+      setSuccess(data.data.data);
+    },
   });
   if (onelineQuery.isLoading) {
     return <Spinner />;
   }
-  if (onelineQuery.data.data.data == 0) {
-    return "";
-  }
 
   return (
     <div className="mt-6 pb-6 ">
-      <div className=" pr-8 pt-0 pb-4 rounded-3xl bg-mGray ">
-        <section className="text-gray-600 body-font ml-10 overflow-hidden">
-          {onelineQuery?.data.data.data.map((line) => (
-            <Oneline {...line} key={line.reviewId} />
-          ))}
-        </section>
-      </div>
+      {success.length > 0 ? (
+        <>
+          <div className=" pr-8 pt-0 pb-4 rounded-3xl bg-mGray ">
+            <section className="text-gray-600 body-font ml-10 overflow-hidden">
+              {onelineQuery?.data.data.data.map((line) => (
+                <Oneline {...line} key={line.reviewId} />
+              ))}
+            </section>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className=" pr-8 pt-10 pb-10 rounded-3xl bg-mGray ">
+            <section className="text-mYellow text-xl body-font ml-10 overflow-hidden">
+              <p>댓글이 없어요🥲</p>
+              <p>첫 한줄평을 남겨주세요</p>
+            </section>
+          </div>
+        </>
+      )}
     </div>
   );
 }
