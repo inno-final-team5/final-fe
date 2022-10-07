296ac1c31d32dc531beb47d2744fb5688faa7f25
woongsnote <mjw92@kakao.com>
2022-10-07 21:52:38 +0900

    style: Table UI 수정

--- src/components/community/TableItem.jsx
+++ src/components/community/TableItem.jsx
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 const TableItem = ({ post }) => {
   return (
     <tr key={post.postId}>
-      <td className="py-4 px-6 font-medium text-mWhite whitespace-nowrap w-12">
+      <td className="py-4 px-3  text-mWhite whitespace-nowrap  ">
         {post.postCategory}
       </td>
-      <td className="py-4 px-6 truncate ">
+      <td className="py-4 px-6 truncate">
         <Link
           to={`/community/detail/${post.postId}`}
           className="hover:text-mYellow"
@@ -15,8 +15,8 @@ const TableItem = ({ post }) => {
           {post.postTitle}
         </Link>
       </td>
-      <td className="py-4 px-6 w-5 truncate">{post.nickname}</td>
-      <td className="py-4 px-6 w-5">
+      <td className="py-4 px-3 whitespace-nowrap w-[400px]">{post.nickname}</td>
+      <td className="py-4 px-2 w-4">
         {new Date(post.createdAt).toLocaleDateString("ko-KR")}
       </td>
     </tr>
