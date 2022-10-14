fb7a291cb02b820f38ab5c70ea9980f01b5f50cf
woongsnote <mjw92@kakao.com>
2022-10-14 10:14:42 +0900

    feat: https 적용

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,14 +1,20 @@
 import axios from "axios";
 
+const isProduction = process.env.NODE_ENV === "production";
+const developmentUrl = `${process.env.REACT_APP_API_URL}`;
+const productionUrl = `${process.env.REACT_APP_API_DOMAIN}`;
+
+const BASE_URL = isProduction ? productionUrl : developmentUrl;
+
 export const api = axios.create({
-  baseURL: "http://13.124.170.188/",
+  baseURL: BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
 });
 
 export const authApi = axios.create({
-  baseURL: "http://13.124.170.188/",
+  baseURL: BASE_URL,
   headers: {
     authorization: localStorage.getItem("accessToken"),
     "refresh-token": localStorage.getItem("refreshToken"),
