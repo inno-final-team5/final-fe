5c55e0f00b816893c12b4fd39cc5f93f8fab2e39
heyley <hyelimiam@gmail.com>
2022-10-16 18:59:10 +0900

    style:css

--- src/components/login/LoginForm.jsx
+++ src/components/login/LoginForm.jsx
@@ -7,7 +7,7 @@ import { useMutation } from "react-query";
 import { Toast } from "components/common/Toast";
 
 const LoginForm = () => {
-  const REDIRECT_URI = "https://moviecritic.site/kakaoLogin";
+  const REDIRECT_URI = "https://www.moviecritic.site/kakaoLogin";
   //const REST_API_KEY = process.env.REACT_APP_KAKAO_ID
   const REST_API_KEY = "3ad9053f0b013a449d0f5d06dfb86796";
   const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
@@ -126,14 +126,14 @@ const LoginForm = () => {
                       return loginUser.mutate(data);
                     }
                   }}
-                  className="flex mx-auto w-26 h-8 text-base md:mt-6 xl:w-1/3 md:w-1/3 sm:w-1/2 place-content-center rounded-lg bg-mYellow border-0 py-1 lg:py-2 lg:h-10 px-10 focus:outline-none hover:bg-mCream  text-lg"
+                  className="flex mx-auto w-26 h-10 text-base md:mt-6 xl:w-1/3 md:w-1/3 sm:w-1/2 place-content-center rounded-lg bg-mYellow border-0 py-2 lg:h-10 px-10 focus:outline-none hover:bg-mCream  text-lg"
                 >
                   로그인
                 </button>
               </div>
               <div className="pt-10 pb-8 w-full">
                 <a href={KAKAO_AUTH_URL}>
-                  <img src={kakao_login} alt="카카오로그인" className="2xl:w-1/2 xl:w-1/2 lg:w-1/2 md:w-1/2 sm:w-1/2 w-1/2 flex mx-auto"></img>
+                  <img src={kakao_login} alt="카카오로그인" className="h-9 2xl:w-1/2 xl:w-1/2 lg:w-1/2 md:w-1/2 sm:w-1/2 w-1/2 flex mx-auto"></img>
                 </a>
               </div>
               <div className="p-2 w-full text-center ">
