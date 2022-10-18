701beee91b1baebf48951d6a79de94c699783499
woongsnote <mjw92@kakao.com>
2022-10-18 21:23:26 +0900

    fix: 리덕스 툴킷 제거

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import { Provider } from "react-redux";
-// import store from "./redux/config/configStore";
 
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -17,14 +15,11 @@ const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* // <Provider store={store}> */}
-
     <GlobalStyle />
     <QueryClientProvider client={queryClient}>
       <App />
       <ReactQueryDevtools initialIsOpen />
     </QueryClientProvider>
-    {/* // </Provider> */}
   </React.StrictMode>
 );
 
