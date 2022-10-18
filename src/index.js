6f30e433f3355a4a0624cb0ba8dd80b8a3b331c4
woongsnote <mjw92@kakao.com>
2022-10-18 20:46:10 +0900

    feat: 상세페이지 뒤로 가기 추가
    - redux 제거
    - 메인페이지 오류 제거

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import store from "./redux/config/configStore";
+// import { Provider } from "react-redux";
+// import store from "./redux/config/configStore";
 
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -16,15 +16,16 @@ const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <React.StrictMode>
-  <Provider store={store}>
+  <React.StrictMode>
+    {/* // <Provider store={store}> */}
+
     <GlobalStyle />
     <QueryClientProvider client={queryClient}>
       <App />
       <ReactQueryDevtools initialIsOpen />
     </QueryClientProvider>
-  </Provider>
-  // </React.StrictMode>
+    {/* // </Provider> */}
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
