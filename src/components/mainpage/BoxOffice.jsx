4f50c0eb4f1d677c35caccfb2f3edb042e55e7ad
Yoo Mina <110373500+maydec2298@users.noreply.github.com>
2022-10-19 14:07:50 +0900

    style : 박스오피스 캐로셀 반응형 수정

--- src/components/mainpage/BoxOffice.jsx
+++ src/components/mainpage/BoxOffice.jsx
@@ -24,18 +24,21 @@ const BoxOffice = () => {
         breakpoint: 1300,
         settings: {
           slidesToShow: 4,
+          slidesToScroll: 2,
         },
       },
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 2,
+          slidesToScroll: 1,
         },
       },
       {
         breakpoint: 640,
         settings: {
           slidesToShow: 1,
+          slidesToScroll: 1,
         },
       },
     ],
