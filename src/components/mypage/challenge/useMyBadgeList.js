34f5ec91516501404ced209da513270294ff672e
woongsnote <mjw92@kakao.com>
2022-10-26 22:36:00 +0900

    refactor: 코드 정리

--- src/components/mypage/challenge/useMyBadgeList.js
+++
@@ -1,8 +0,0 @@
-import { getMyBadges } from "apis/badgeApi";
-import { useQuery } from "react-query";
-
-function useMyBadgeList() {
-  return useQuery("badgeList", getMyBadges, { keepPreviousData: true });
-}
-
-export default useMyBadgeList;
